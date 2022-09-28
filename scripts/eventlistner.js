49d99abacb80b9caa9f005c611fdaada71646dd9
RittikPradhan <rittikpradhan5@gmail.com>
2022-09-28 10:04:38 +0530

    feat: event listner on transfer

--- scripts/eventlistner.js
+++ scripts/eventlistner.js
@@ -1,42 +1,37 @@
-const hre = require('hardhat');
-const { ethers } = require("hardhat");
-var Web3 = require('web3');
-require('dotenv').config();
+// const hre = require('hardhat');
+// const { ethers } = require("hardhat");
+var Web3 = require("web3");
+require("dotenv").config();
 
-async function main() {
+const ALCHEMY_Provider = `${process.env.ALCHEMY_RINKEBY_URL}`;
+const web3 = new Web3(new Web3.providers.WebsocketProvider(ALCHEMY_Provider));
 
-    // const NFTContract = await (await hre.ethers.getContractFactory("NFTContract"))
-    //     .attach("0xd9145CCE52D386f254917e481eB44e9943F39138");  -- local
-    const NFTContract = await (await hre.ethers.getContractFactory("NFTContract"))
-        .attach("0x7C455c0610C0c2Beb857eC522983C0da7BB8A147");
+const contractAddress = "0x7C455c0610C0c2Beb857eC522983C0da7BB8A147";
+const contractAbi = require("../build/abi.json");
 
-    const ALCHEMY_Provider = `${process.env.ALCHEMY_RINKEBY_URL}`;
-    var web3 = new Web3(new Web3.providers.HttpProvider(ALCHEMY_Provider));
-    var blockNumber = await (await web3.eth.getBlock("latest")).number;
-    console.log(blockNumber, "is the current block no.");
+const NFTContract = new web3.eth.Contract(contractAbi, contractAddress);
+// var blockNumber = web3.eth.getBlock("latest");
+// web3.eth.getBlock("latest").then(console.log);
+// console.log(blockNumber, "is the current block no.");
 
-    // NFTContract.interface.events.TransferSingle({
-    //     fromBlock: blockNumber
-    // }, (error, event) => {
-    //     console.log(event);
-    // })
-    // .on("connected", (subscriptionId) => {
-    //     console.log(subscriptionId)
-    // })
-    // .on('data', (event) => {
-    //     console.log(event)
-    // })
-    // .on('changed', (event) => {
-    //     //remove event from local database
-    // })
-    // .on('error', (error, receipt) => {
-    //     // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt....
-    // });
-}
-
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+NFTContract.events
+  .TransferSingle(
+    // {
+    //   fromBlock: blockNumber,
+    // },
+    (error, event) => {
+      console.log(event);
+    }
+  )
+  .on("connected", (subscriptionId) => {
+    console.log({subscriptionId});
+  })
+  .on("data", (event) => {
+    console.log(event);
+  })
+  .on("changed", (event) => {
+    //remove event from local database
+  })
+  .on("error", (error, receipt) => {
+    // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt....
+  });
