f80a17d140563cdcc738c77ac19bc0d487a33d61
RittikPradhan <rittikpradhan5@gmail.com>
2022-09-28 19:32:09 +0530

    feat: listing Databases

--- scripts/eventlistner.js
+++ scripts/eventlistner.js
@@ -1,6 +1,9 @@
 // const hre = require('hardhat');
 // const { ethers } = require("hardhat");
-var Web3 = require("web3");
+
+const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
+const Web3 = require("web3");
 require("dotenv").config();
 
 const ALCHEMY_Provider = `${process.env.ALCHEMY_RINKEBY_URL}`;
@@ -14,24 +17,62 @@ const NFTContract = new web3.eth.Contract(contractAbi, contractAddress);
 // web3.eth.getBlock("latest").then(console.log);
 // console.log(blockNumber, "is the current block no.");
 
-NFTContract.events
-  .TransferSingle(
-    // {
-    //   fromBlock: blockNumber,
-    // },
-    (error, event) => {
-      console.log(event);
-    }
-  )
-  .on("connected", (subscriptionId) => {
-    console.log({subscriptionId});
-  })
-  .on("data", (event) => {
-    console.log(event);
-  })
-  .on("changed", (event) => {
-    //remove event from local database
-  })
-  .on("error", (error, receipt) => {
-    // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt....
-  });
+const uri =
+  "mongodb+srv://0xrittikpradhan:s3ni79lQcElpJS4v@cluster0.fuglox2.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+// await mongoose.connect("mongodb+srv://0xrittikpradhan:s3ni79lQcElpJS4v@cluster0.fuglox2.mongodb.net/?retryWrites=true&w=majority");
+
+try {
+  //   console.log(client);
+  NFTContract.events
+    .TransferSingle(
+      // {
+      //   fromBlock: blockNumber,
+      // },
+      (error, event) => {
+        console.log("Event Listen: Success");
+      }
+    )
+    .on("connected", (subscriptionId) => {
+      console.log({ subscriptionId });
+    })
+    .on("data", (event) => {
+
+      const eventDetails = {
+        "Block_Number" : event.blockNumber.toString(),
+        "Event_Name" : event.event.toString(),
+        "Operator_Address" : event.returnValues.operator.toString(),
+        "From_Address" : event.returnValues.from.toString(),
+        "To_Address" : event.returnValues.to.toString(),
+        "Token_Id" : event.returnValues.id.toString(),
+        "Token_Amount" : event.returnValues.value.toString()
+      };
+      console.log(eventDetails);
+        await client.connect();
+      
+        await createListing(client, eventDetails);
+    })
+    .on("changed", (event) => {
+      //remove event from local database
+    })
+    .on("error", (error, receipt) => {
+      // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt....
+    });
+} catch (e) {
+  console.error(e);
+} finally {
+    await client.close();
+}
+
+async function createListing(client, event) {
+  const result = await client
+    .db("Addresses")
+    .collection("SingleTransferEvent")
+    .insert(event);
+  console.log(
+    "New listing created with the following id: ${result.insertedId}"
+  );
+}
+
+// main().catch(console.error);
